a037bfd4e8dcf30d28b39532a3de930c4a2022e0
weiyang <15259070752@163.com>
2025-07-15 17:02:55 +0800

    添加脱敏注解和各种实现

---
+++ neaterp-framework/neaterp-spring-boot-starter-web/src/main/java/com/neaterp/framework/desensitize/core/regex/handler/DefaultRegexDesensitizationHandler.java
@@ -0,0 +1,28 @@
+package com.neaterp.framework.desensitize.core.regex.handler;
+
+
+import com.neaterp.framework.desensitize.core.regex.annotation.RegexDesensitize;
+
+/**
+ * {@link RegexDesensitize} 的正则脱敏处理器
+ *
+ * @author gaibu
+ */
+public class DefaultRegexDesensitizationHandler extends AbstractRegexDesensitizationHandler<RegexDesensitize> {
+
+    @Override
+    String getRegex(RegexDesensitize annotation) {
+        return annotation.regex();
+    }
+
+    @Override
+    String getReplacer(RegexDesensitize annotation) {
+        return annotation.replacer();
+    }
+
+    @Override
+    public String getDisable(RegexDesensitize annotation) {
+        return annotation.disable();
+    }
+
+}
---
+++ neaterp-framework/neaterp-spring-boot-starter-web/src/main/java/com/neaterp/framework/desensitize/core/slider/handler/BankCardDesensitization.java
@@ -0,0 +1,33 @@
+package com.neaterp.framework.desensitize.core.slider.handler;
+
+
+import com.neaterp.framework.desensitize.core.slider.annotation.BankCardDesensitize;
+
+/**
+ * {@link BankCardDesensitize} 的脱敏处理器
+ *
+ * @author gaibu
+ */
+public class BankCardDesensitization extends AbstractSliderDesensitizationHandler<BankCardDesensitize> {
+
+    @Override
+    Integer getPrefixKeep(BankCardDesensitize annotation) {
+        return annotation.prefixKeep();
+    }
+
+    @Override
+    Integer getSuffixKeep(BankCardDesensitize annotation) {
+        return annotation.suffixKeep();
+    }
+
+    @Override
+    String getReplacer(BankCardDesensitize annotation) {
+        return annotation.replacer();
+    }
+
+    @Override
+    public String getDisable(BankCardDesensitize annotation) {
+        return "";
+    }
+
+}
