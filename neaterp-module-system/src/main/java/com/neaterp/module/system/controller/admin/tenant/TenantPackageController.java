dcf485f1d82ac837ccebfbe9d507ce003ce0d691
weiyang <15259070752@163.com>
2025-08-13 11:31:56 +0800

    租户套餐管理

---
+++ neaterp-module-system/src/main/java/com/neaterp/module/system/controller/admin/tenant/TenantPackageController.java
@@ -0,0 +1,17 @@
+package com.neaterp.module.system.controller.admin.tenant;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "管理后台 - 租户套餐")
+@RestController
+@RequestMapping("/system/tenant-package")
+@Validated
+public class TenantPackageController {
+
+
+
+
+}
