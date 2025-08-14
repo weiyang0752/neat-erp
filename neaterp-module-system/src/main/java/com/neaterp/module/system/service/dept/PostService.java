5fdc3a3b31624dbc0e212ed97fc755c1af10e985
weiyang <15259070752@163.com>
2025-08-14 10:05:01 +0800

    岗位管理

--- neaterp-module-system/src/main/java/com/neaterp/module/system/service/dept/PostService.java
+++ neaterp-module-system/src/main/java/com/neaterp/module/system/service/dept/PostService.java
@@ -1,8 +1,13 @@
 package com.neaterp.module.system.service.dept;
 
+import com.neaterp.framework.common.pojo.PageResult;
+import com.neaterp.module.system.controller.admin.dept.vo.post.PostPageReqVO;
+import com.neaterp.module.system.controller.admin.dept.vo.post.PostSaveReqVO;
 import com.neaterp.module.system.dal.dataobject.dept.PostDO;
+import org.springframework.lang.Nullable;
 
 import java.util.Collection;
+import java.util.List;
 
 /**
  * 岗位 Service 接口
@@ -12,6 +17,28 @@ import java.util.Collection;
 public interface PostService {
 
     /**
+     * 创建岗位
+     *
+     * @param createReqVO 岗位信息
+     * @return 岗位编号
+     */
+    Long createPost(PostSaveReqVO createReqVO);
+
+    /**
+     * 更新岗位
+     *
+     * @param updateReqVO 岗位信息
+     */
+    void updatePost(PostSaveReqVO updateReqVO);
+
+    /**
+     * 删除岗位信息
+     *
+     * @param id 岗位编号
+     */
+    void deletePost(Long id);
+
+    /**
      * 获得岗位信息
      *
      * @param id 岗位编号
@@ -19,7 +46,23 @@ public interface PostService {
      */
     PostDO getPost(Long id);
 
+    /**
+     * 获得符合条件的岗位列表
+     *
+     * @param ids 岗位编号数组。如果为空，不进行筛选
+     * @param statuses 状态数组。如果为空，不进行筛选
+     * @return 部门列表
+     */
+    List<PostDO> getPostList(@Nullable Collection<Long> ids,
+                             @Nullable Collection<Integer> statuses);
 
+    /**
+     * 获得岗位分页列表
+     *
+     * @param reqVO 分页条件
+     * @return 部门分页列表
+     */
+    PageResult<PostDO> getPostPage(PostPageReqVO reqVO);
 
     /**
      * 校验岗位们是否有效。如下情况，视为无效：
--- neaterp-module-system/src/main/java/com/neaterp/module/system/service/dept/PostServiceImpl.java
+++ neaterp-module-system/src/main/java/com/neaterp/module/system/service/dept/PostServiceImpl.java
@@ -2,6 +2,10 @@ package com.neaterp.module.system.service.dept;
 
 import cn.hutool.core.collection.CollUtil;
 import com.neaterp.framework.common.enums.CommonStatusEnum;
+import com.neaterp.framework.common.pojo.PageResult;
+import com.neaterp.framework.common.util.object.BeanUtils;
+import com.neaterp.module.system.controller.admin.dept.vo.post.PostPageReqVO;
+import com.neaterp.module.system.controller.admin.dept.vo.post.PostSaveReqVO;
 import com.neaterp.module.system.dal.dataobject.dept.PostDO;
 import com.neaterp.module.system.dal.mysql.dept.PostMapper;
 import jakarta.annotation.Resource;
@@ -14,8 +18,7 @@ import java.util.Map;
 
 import static com.neaterp.framework.common.exception.util.ServiceExceptionUtil.exception;
 import static com.neaterp.framework.common.util.collection.CollectionUtils.convertMap;
-import static com.neaterp.module.system.enums.ErrorCodeConstants.POST_NOT_ENABLE;
-import static com.neaterp.module.system.enums.ErrorCodeConstants.POST_NOT_FOUND;
+import static com.neaterp.module.system.enums.ErrorCodeConstants.*;
 
 /**
  * 岗位 Service 接口
@@ -31,8 +34,56 @@ public class PostServiceImpl implements PostService {
 
 
     @Override
+    public Long createPost(PostSaveReqVO createReqVO) {
+        // 校验正确性
+        validatePostForCreateOrUpdate(null, createReqVO.getName(), createReqVO.getCode());
+
+        // 插入岗位
+        PostDO post = BeanUtils.toBean(createReqVO, PostDO.class);
+        postMapper.insert(post);
+        return post.getId();
+    }
+
+    @Override
+    public void updatePost(PostSaveReqVO updateReqVO) {
+        // 校验正确性
+        validatePostForCreateOrUpdate(updateReqVO.getId(), updateReqVO.getName(), updateReqVO.getCode());
+
+        // 更新岗位
+        PostDO updateObj = BeanUtils.toBean(updateReqVO, PostDO.class);
+        postMapper.updateById(updateObj);
+    }
+
+    @Override
+    public void deletePost(Long id) {
+        // 校验是否存在
+        validatePostExists(id);
+        // 删除岗位
+        postMapper.deleteById(id);
+    }
+
+    @Override
     public PostDO getPost(Long id) {
         return postMapper.selectById(id);
+    }
+
+    @Override
+    public List<PostDO> getPostList(Collection<Long> ids, Collection<Integer> statuses) {
+        return postMapper.selectList(ids, statuses);
+    }
+
+    @Override
+    public PageResult<PostDO> getPostPage(PostPageReqVO reqVO) {
+        return postMapper.selectPage(reqVO);
+    }
+
+    private void validatePostForCreateOrUpdate(Long id, String name, String code) {
+        // 校验自己存在
+        validatePostExists(id);
+        // 校验岗位名的唯一性
+        validatePostNameUnique(id, name);
+        // 校验岗位编码的唯一性
+        validatePostCodeUnique(id, code);
     }
 
     @Override
@@ -54,4 +105,44 @@ public class PostServiceImpl implements PostService {
             }
         });
     }
+
+    private void validatePostNameUnique(Long id, String name) {
+        PostDO post = postMapper.selectByName(name);
+        if (post == null) {
+            return;
+        }
+        // 如果 id 为空，说明不用比较是否为相同 id 的岗位
+        if (id == null) {
+            throw exception(POST_NAME_DUPLICATE);
+        }
+        if (!post.getId().equals(id)) {
+            throw exception(POST_NAME_DUPLICATE);
+        }
+    }
+
+    private void validatePostCodeUnique(Long id, String code) {
+        PostDO post = postMapper.selectByCode(code);
+        if (post == null) {
+            return;
+        }
+        // 如果 id 为空，说明不用比较是否为相同 id 的岗位
+        if (id == null) {
+            throw exception(POST_CODE_DUPLICATE);
+        }
+        if (!post.getId().equals(id)) {
+            throw exception(POST_CODE_DUPLICATE);
+        }
+    }
+
+    private void validatePostExists(Long id) {
+        if (id == null) {
+            return;
+        }
+        if (postMapper.selectById(id) == null) {
+            throw exception(POST_NOT_FOUND);
+        }
+    }
+
+
+
 }
