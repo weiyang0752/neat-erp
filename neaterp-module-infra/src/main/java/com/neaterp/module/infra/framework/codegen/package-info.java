bddc57c88cf63948e903ce18d63083d112796eca
zwy <15259070752@163.com>
2025-09-07 23:02:27 +0800

    代码生成器

---
+++ neaterp-module-infra/src/main/java/com/neaterp/module/infra/framework/codegen/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 代码生成器
+ */
+package com.neaterp.module.infra.framework.codegen;
--- sql/mysql/neaterp.sql
+++ sql/mysql/neaterp.sql
@@ -3369,5 +3369,82 @@ BEGIN;
 COMMIT;
 
 
+-- ----------------------------
+-- Table structure for infra_codegen_column
+-- ----------------------------
+DROP TABLE IF EXISTS `infra_codegen_column`;
+CREATE TABLE `infra_codegen_column`  (
+    `id` bigint NOT NULL AUTO_INCREMENT COMMENT '编号',
+    `table_id` bigint NOT NULL COMMENT '表编号',
+    `column_name` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '字段名',
+    `data_type` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '字段类型',
+    `column_comment` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '字段描述',
+    `nullable` bit(1) NOT NULL COMMENT '是否允许为空',
+    `primary_key` bit(1) NOT NULL COMMENT '是否主键',
+    `ordinal_position` int NOT NULL COMMENT '排序',
+    `java_type` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Java 属性类型',
+    `java_field` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Java 属性名',
+    `dict_type` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '' COMMENT '字典类型',
+    `example` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '数据示例',
+    `create_operation` bit(1) NOT NULL COMMENT '是否为 Create 创建操作的字段',
+    `update_operation` bit(1) NOT NULL COMMENT '是否为 Update 更新操作的字段',
+    `list_operation` bit(1) NOT NULL COMMENT '是否为 List 查询操作的字段',
+    `list_operation_condition` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '=' COMMENT 'List 查询操作的条件类型',
+    `list_operation_result` bit(1) NOT NULL COMMENT '是否为 List 查询操作的返回字段',
+    `html_type` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '显示类型',
+    `creator` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '' COMMENT '创建者',
+    `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updater` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '' COMMENT '更新者',
+    `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `deleted` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2538 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '代码生成表字段定义';
+
+-- ----------------------------
+-- Records of infra_codegen_column
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
+-- Table structure for infra_codegen_table
+-- ----------------------------
+DROP TABLE IF EXISTS `infra_codegen_table`;
+CREATE TABLE `infra_codegen_table`  (
+    `id` bigint NOT NULL AUTO_INCREMENT COMMENT '编号',
+    `data_source_config_id` bigint NOT NULL COMMENT '数据源配置的编号',
+    `scene` tinyint NOT NULL DEFAULT 1 COMMENT '生成场景',
+    `table_name` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '表名称',
+    `table_comment` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '表描述',
+    `remark` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '备注',
+    `module_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '模块名',
+    `business_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '业务名',
+    `class_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '类名称',
+    `class_comment` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '类描述',
+    `author` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '作者',
+    `template_type` tinyint NOT NULL DEFAULT 1 COMMENT '模板类型',
+    `front_type` tinyint NOT NULL COMMENT '前端类型',
+    `parent_menu_id` bigint NULL DEFAULT NULL COMMENT '父菜单编号',
+    `master_table_id` bigint NULL DEFAULT NULL COMMENT '主表的编号',
+    `sub_join_column_id` bigint NULL DEFAULT NULL COMMENT '子表关联主表的字段编号',
+    `sub_join_many` bit(1) NULL DEFAULT NULL COMMENT '主表与子表是否一对多',
+    `tree_parent_column_id` bigint NULL DEFAULT NULL COMMENT '树表的父字段编号',
+    `tree_name_column_id` bigint NULL DEFAULT NULL COMMENT '树表的名字字段编号',
+    `creator` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '' COMMENT '创建者',
+    `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updater` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '' COMMENT '更新者',
+    `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `deleted` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 191 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '代码生成表定义';
+
+-- ----------------------------
+-- Records of infra_codegen_table
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+
+
 
 SET FOREIGN_KEY_CHECKS = 1;
