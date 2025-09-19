50d6be57895429c197590b70dde0dab78e3c6c46
weiyang <15259070752@163.com>
2025-09-19 17:01:48 +0800

    bpm配置

---
+++ neaterp-module-bpm/src/main/java/com/neaterp/module/bpm/framework/web/config/BpmWebConfiguration.java
@@ -0,0 +1,24 @@
+package com.neaterp.module.bpm.framework.web.config;
+
+import com.neaterp.framework.swagger.config.NeaterpSwaggerAutoConfiguration;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * bpm 模块的 web 组件的 Configuration
+ *
+ * @author 芋道源码
+ */
+@Configuration(proxyBeanMethods = false)
+public class BpmWebConfiguration {
+
+    /**
+     * bpm 模块的 API 分组
+     */
+    @Bean
+    public GroupedOpenApi bpmGroupedOpenApi() {
+        return NeaterpSwaggerAutoConfiguration.buildGroupedOpenApi("bpm");
+    }
+
+}
