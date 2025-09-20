073681635fcc9b0080e7eccf0226ccf8e182db48
zwy <15259070752@163.com>
2025-09-20 15:31:54 +0800

    bpm监听器

--- neaterp-module-bpm/src/main/java/com/neaterp/module/bpm/dal/dataobject/definition/BpmProcessListenerDO.java
+++ neaterp-module-bpm/src/main/java/com/neaterp/module/bpm/dal/dataobject/definition/BpmProcessListenerDO.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.KeySequence;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.neaterp.framework.mybatis.core.dataobject.BaseDO;
+import com.neaterp.module.bpm.enums.definition.BpmProcessListenerTypeEnum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
